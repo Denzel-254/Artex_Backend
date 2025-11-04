4ebce83f4af962465d6b4274b7a4f7eac9a21d4c
Denzel-254 <ochiengden335@gmail.com>
2025-11-05 00:09:07 +0300

    Modified the userschema

--- models/usermodel.js
+++ models/usermodel.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
     userName: { type: String, required: [true, "username is required"] },
@@ -9,6 +9,8 @@ const userSchema = new mongoose.Schema({
 )
 
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+
+export default User
 
 
